4d8908a3d59c9358351357888b527725ce697ae9
Kajal-techy <kajalmathur05@gmail.com>
2020-05-27 11:02:56 -0400

    added@JsonignoreProperties for exception messages

--- src/main/java/com/userservice/exception/UserExistsException.java
+++ src/main/java/com/userservice/exception/UserExistsException.java
@@ -1,5 +1,8 @@
 package com.userservice.exception;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({ "stackTrace", "cause", "suppressed", "localizedMessage"})
 public class UserExistsException extends RuntimeException {
 
     public UserExistsException(String message) {
