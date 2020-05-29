ba90170abb9fcb4d97ecd17d2de502bab550e801
Kajal-techy <kajalmathur05@gmail.com>
2020-05-29 16:40:08 -0400

    swagger implementation and transactional

--- src/main/java/com/userservice/model/User.java
+++ src/main/java/com/userservice/model/User.java
@@ -1,5 +1,6 @@
 package com.userservice.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,20 +15,26 @@ import org.springframework.lang.NonNull;
 public class User implements Cloneable {
 
     @Id
+    @ApiModelProperty(notes = "The database generated User ID")
     private String id;
 
     @NonNull
+    @ApiModelProperty(notes = "User FirstName ")
     private String firstName;
 
     @NonNull
+    @ApiModelProperty(notes = "User lastName ")
     private String lastName;
 
     @NonNull
+    @ApiModelProperty(notes = "User userName ")
     private String userName;
 
     @NonNull
+    @ApiModelProperty(notes = "User password ")
     private String password;
 
+    @ApiModelProperty(notes = "User address ")
     private Address address;
 
     @Override
--- src/main/java/com/userservice/service/UserServiceImpl.java
+++ src/main/java/com/userservice/service/UserServiceImpl.java
@@ -7,12 +7,14 @@ import com.userservice.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
